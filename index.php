dd7a8d6c03b9b4e1066a0307b78343b3e7a6a7c2
Icas Velaninda <icasvee@gmail.com>
2022-10-26 14:26:21 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Icas";
+$umur = 20;
+
+echo "Nama saya <b>$nama</b>, saya berusia $umur tahun.";
+
+$namaAdik = "Jhon";
+$umurAdik = 19;
+
+echo "<br>Nama adik saya <b>$namaAdik</b>, adik saya berusia $umurAdik tahun.";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "<br>Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
